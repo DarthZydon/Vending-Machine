9cca055d2ac30f8577ff171e31fa2cf86e7270c4
Zachary Ford <zacharyford13@gmail.com>
2022-02-10 16:08:25 -0500

    Date Time Formatter

---
+++ src/main/java/com/techelevator/view/TransactionLog.java
@@ -0,0 +1,14 @@
+package com.techelevator.view;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TransactionLog {
+    public void time(){
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("dd/MM/YYYY HH:mm:ss");
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time.format(formatter));
+    }
+}
---
+++ src/test/java/com/techelevator/view/TransactionLogTest.java
@@ -0,0 +1,13 @@
+package com.techelevator.view;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionLogTest {
+
+    @Test
+    public void time() {
+
+    }
+}
