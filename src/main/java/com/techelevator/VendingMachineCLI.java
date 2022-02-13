4467c73664a54afe9bb5d29a70ea1c2ccf5c8d89
Neil Haleem <neil.haleem01@gmail.com>
2022-02-13 15:11:32 -0500

    neil upload 9

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -6,49 +6,61 @@ import java.io.FileNotFoundException;
 import java.util.Map;
 import java.util.Scanner;
 
-public class VendingMachineCLI{
+public class VendingMachineCLI {
 
-
+	private Scanner in = new Scanner(System.in);
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit Main Menu";
+	private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT};
+
+
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Deposit Money";
 	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
 	private static final String PURCHASE_MENU_OPTION_END_TRANSACTION = "End Transaction";
-	private static final String PURCHASE_MENU_OPTION_DEPOSIT_MONEY = "Deposit Money";
-
-	private static final String DEPOSIT_MENU_OPTIONS_DOLLAR_AMOUNT = "Please Deposit $1, $5, or $10";
-	private static final String DEPOSIT_MENU_OPTIONS_EXIT = "Exit from Deposit Menu";
-
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
-	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_DEPOSIT_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_END_TRANSACTION };
-	private static final String[] DEPOSIT_MENU_OPTIONS = { DEPOSIT_MENU_OPTIONS_DOLLAR_AMOUNT, DEPOSIT_MENU_OPTIONS_EXIT };
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_END_TRANSACTION};
 
 
 	private Menu menu;
-//	private VendingMachine vendingMachine = new VendingMachine("vendingmachine.csv");
 
+//	private VendingMachine vendingMachine = new VendingMachine("vendingmachine.csv");
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
 	}
 
-	private boolean getChoiceFromOptions() {
-		return false;
+//	private boolean getChoiceFromOptions() {
+//		return false;
+//	}
+
+	private void showProducts(Inventory inventory) throws FileNotFoundException {
+		Map<String, Product> productMap = inventory.getInventory();
+		for (Map.Entry<String, Product> entry : productMap.entrySet()) {
+			Product value = entry.getValue();
+			System.out.println(entry.getKey() + " " + value.getName() + " " + value.getPrice() + " " + value.getCount());
+		}
 	}
 
-	String[] activeMenu = MAIN_MENU_OPTIONS;
+	public void run() throws FileNotFoundException {
+		VendingMachine vm = new VendingMachine();
+		Inventory inventory = vm.getInventory();
+		inventory.load();
 
-	public String run() {
-		while (true) {
+		String[] activeMenu = MAIN_MENU_OPTIONS;
+
+		boolean run = true;
+
+		while (run) {
 			String choice = (String) menu.getChoiceFromOptions(activeMenu);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+				showProducts(inventory);
 				// display vending machine items
-				Map<String, Product> workingInv = vendingMachine.getInventory().inventory;
-				for (Map.Entry<String, Product> entry : workingInv.entrySet()) {
-					System.out.println(entry.getKey() + " " + entry.getValue().getName() + " " + entry.getValue().getPrice());
-				}
+//				Map<String, Product> workingInv = vendingMachine.getInventory().inventory;
+//				for (Map.Entry<String, Product> entry : workingInv.entrySet()) {
+//					System.out.println(entry.getKey() + " " + entry.getValue().getName() + " " + entry.getValue().getPrice());
+//				}
 
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// deposit money
@@ -59,20 +71,71 @@ public class VendingMachineCLI{
 //			} else if (choice.equals(PURCHASE_MENU_OPTION_DEPOSIT_MONEY)) {
 //				vendingMachine.feedMoney();
 
-			} else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
-					// vendingMachine.selectProduct(getChoiceFromOptions());
-					System.out.println("select product code");
+			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
+				// vendingMachine.selectProduct(getChoiceFromOptions());
+				run = false;
 
-			} else if (choice.equals(PURCHASE_MENU_OPTION_END_TRANSACTION)) {
-				activeMenu = MAIN_MENU_OPTIONS;
+			} else if (choice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
+				System.out.println("Deposit Money in Whole Dollar Amounts");
+				String deposit = in.nextLine();
+				vm.feedMoney(Integer.parseInt(deposit));
+				if (vm.getCustomerBalance() == 0) {
+					System.out.println("Must Deposit Money Before Making Selection");
+				} else if (vm.getCustomerBalance() > 0) {
+					System.out.println("Current Balance: " + vm.getCustomerBalance() / 100.0);
 				}
+
+			} else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+				System.out.println("Enter Product ID: ");
+				showProducts(inventory);
+				String slotID = in.nextLine();
+				try {
+					Product product = vm.selectProduct(slotID);
+					System.out.println("Thanks for your Purchase of: " + product.getName() + " " + product.getPrice() + " " + vm.getCustomerBalance());
+					if (product.getCategory().equalsIgnoreCase("chips")) {
+						System.out.println("Crunch, Crunch, Yum!");
+					} else if (product.getCategory().equalsIgnoreCase("candy")) {
+						System.out.println("Munch, Munch, Yum!");
+					} else if (product.getCategory().equalsIgnoreCase("drink")) {
+						System.out.println("Glug, Glug, Yum!");
+					} else if (product.getCategory().equalsIgnoreCase("gum")) {
+						System.out.println("Chew, Chew, Yum!");
+					}
+				} catch (VendExceptions vendExceptions) {
+					System.out.println("Sale Failed, Please Try Again");
+				}
+			} else if (choice.equals(PURCHASE_MENU_OPTION_END_TRANSACTION)) {
+				int change = vm.finishTransaction();
+				System.out.println("Change Due: " + change / 100.0);
+			}
+
+			activeMenu = MAIN_MENU_OPTIONS;
+		}
+	}
+
+
+			public static void main(String[] args) throws FileNotFoundException {
+				Menu menu = new Menu(System.in, System.out);
+				VendingMachineCLI cli = new VendingMachineCLI(menu);
+//				try {
+//					cli.vendingMachine.getInventory().load();
+//				} catch (FileNotFoundException e) {
+//					System.out.println("Couldn't load the inventory " + e.getMessage());
+//				}
+				cli.run();
+			}
+		}
+
+
+
+
+
 //
 //			} else if (choice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
 //				vendingMachine.selectProduct()
 //
 //			} else if (choice.equals(PURCHASE_MENU_OPTION_END_TRANSACTION)) {
 //				vendingMachine.finishTransaction();
-			}
 
 //				Product product = null;
 //				try {
@@ -82,17 +145,3 @@ public class VendingMachineCLI{
 //					System.out.println("You can't buy product " + e.getMessage());
 //				}
 
-			}
-
-
-	public static void main(String[] args) {
-		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
-		try {
-			cli.vendingMachine.getInventory().load();
-		} catch (FileNotFoundException e) {
-			System.out.println("Couldn't load the inventory " + e.getMessage());
-		}
-		cli.run();
-	}
-}
